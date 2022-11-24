8b3fc6ed8fb9ee122299a09767429b58ec979d22
ellofae <94999672+ellofae@users.noreply.github.com>
2022-11-24 19:44:02 +0300

    Create HomeController.cs

---
+++ Using Default Implementations in Interfaces/HomeController.cs
@@ -0,0 +1,21 @@
+//using Microsoft.AspNetCore.Mvc;
+//using LanguageFeatures.Models;
+
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            IProductSelection cart = new ShoppingCart(
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer ball", Price = 19.50M },
+                new Product { Name = "Corner flag", Price = 34.95M }
+                );
+            return View(cart.Name);
+        }
+    }
+}
