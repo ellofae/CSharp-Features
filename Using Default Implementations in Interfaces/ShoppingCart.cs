58ae64b5b549fddffeb41c5de75e131c18e7846a
ellofae <94999672+ellofae@users.noreply.github.com>
2022-11-24 19:43:35 +0300

    Create ShoppingCart.cs

---
+++ Using Default Implementations in Interfaces/ShoppingCart.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+
+namespace LanguageFeatures.Models
+{
+    public class ShoppingCart : IProductSelection
+    {
+        private List<Product> products = new();
+
+        public ShoppingCart(params Product[] prods)
+        {
+            products.AddRange(prods);
+        }
+        public IEnumerable<Product>? Products { get => products; }
+
+    }
+}
