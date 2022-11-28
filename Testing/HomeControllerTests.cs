181acda0fae87f6bb319a61feffc4a6db7e7fdc8
ellofae <94999672+ellofae@users.noreply.github.com>
2022-11-28 17:21:10 +0300

    Create HomeControllerTests.cs

---
+++ Testing/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SimpleApp.Controllers;
+using SimpleApp.Models;
+using Xunit;
+
+namespace SimpleApp.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModelIsComplete()
+        {
+            var controller = new HomeController();
+            Product[] products = new Product[] {
+                 new Product { Name = "Kayak", Price = 275M },
+                 new Product { Name = "Lifejacket", Price = 48.95M}
+            };
+
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            Assert.Equal(products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+        }
+    }
+}
